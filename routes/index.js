8df3f1c496fafdfe6143c164f3fd0e309a7b5773
Torkild Sande <trokild@gmail.com>
2024-03-16 12:30:38 +0100

    Task 1.1

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,10 @@ const add = require("../add");
 
 const router = new Router();
 
+router.get("/", function (req, res, next) {
+  res.json({ message: "Hello World" });
+});
+
 router.get("/sum/:number1/:number2", (req, res) => {
   const {number1, number2} = req.params;
   if(number1 == null || number2 == null) {
